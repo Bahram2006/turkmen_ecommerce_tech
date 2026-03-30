8c04128915c5a8be8ded81e7122eefe6bdf26ad6
Bahram <yardam034@gmail.com>
2026-03-30 13:05:10 +0500

    products grid template update

---
+++ src/data/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    id: 1,
+    name: "Gaming Monitor",
+    price: 4500,
+    image: "/icons/gaming-monitor.png",
+    category: "monitor",
+    description: "High quality gaming monitor with 144Hz",
+  },
+  {
+    id: 2,
+    name: "Office Monitor",
+    price: 3200,
+    image: "/icons/monitor.png",
+    category: "monitor",
+    description: "Perfect for office work",
+  },
+  {
+    id: 3,
+    name: "Monoblok",
+    price: 7000,
+    image: "/icons/monoblok.png",
+    category: "computer",
+    description: "All-in-one powerful PC",
+  },
+  {
+    id: 4,
+    name: "Keyboard",
+    price: 500,
+    image: "/icons/keyboard.png",
+    category: "accessory",
+    description: "Mechanical keyboard RGB",
+  },
+];
